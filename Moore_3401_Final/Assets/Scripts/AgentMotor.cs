72ef62177c4023e2b563bbe4c4d171d4deda861e
Hope Moore <hopemoore@gmail.com>
2019-05-02 13:23:26 -0400

    Adding project files

---
+++ Moore_3401_Final/Assets/Scripts/AgentMotor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMotor : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public NavMeshAgent agent;
+    public float agentSpeed = 3.5F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = agentSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(cameraRay, out hit))
+            {
+                targetPosition = new Vector3(hit.point.x, 0, hit.point.z);
+            }
+        }
+
+        agent.destination = targetPosition;
+    }
+}
---
+++ Moore_3401_Final/Assets/Scripts/ObstacleBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehavior : MonoBehaviour
+{
+    public GameObject obstacle;
+    public ParticleSystem obstacleParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstacle = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            obstacleParticles.Play();
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+            obstacleParticles.Stop();
+    }
+}
